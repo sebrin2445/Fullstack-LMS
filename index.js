f9e00155050a897db615f36eca5319dadb6f40bf
¨sebrin2445¨ <sebrinasemir2445@gmail.com>
2024-05-22 21:21:34 +0300

    fixed the navbar and installed some packages

---
+++ index.js
@@ -0,0 +1,14 @@
+
+
+function toggleSidebar(){
+    var sidebar=document.querySelector('.menu-main');
+    const menuItems = document.querySelector('.nav-list');
+
+    sidebar.classList.toggle('open');
+    menuItems.classList.toggle('open');
+
+    
+    
+    
+  }
+  
